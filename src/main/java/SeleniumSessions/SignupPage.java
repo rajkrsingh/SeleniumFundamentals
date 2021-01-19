c1e1697f9f4abd156b7a11e036c30fc1abd54ead
unknown <rajkumasingh@gmail.com>
2021-01-19 19:15:15 +0530

    adding new file signup

---
+++ src/main/java/SeleniumSessions/SignupPage.java
@@ -0,0 +1,16 @@
+package SeleniumSessions;
+
+public class SignupPage {
+	
+	public SignupPage() {
+		
+		System.out.println("This is signup constructor");
+	}
+	
+	public void doSignUp(){
+		System.out.println("This is local method");
+	}
+		
+			
+
+}
