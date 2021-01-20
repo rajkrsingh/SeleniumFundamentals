6e973ce6fdaa07bcc9d6a83b3c5556c93b9545d4
unknown <rajkumasingh@gmail.com>
2021-01-20 20:32:40 +0530

    added xpath and one new method

--- src/main/java/SeleniumSessions/SignupPage.java
+++ src/main/java/SeleniumSessions/SignupPage.java
@@ -1,14 +1,22 @@
 package SeleniumSessions;
 
+import org.openqa.selenium.By;
+
 public class SignupPage {
 	
 	public SignupPage() {
+		
+		By signup= By.id("signup");
 		
 		System.out.println("This is signup constructor");
 	}
 	
 	public void doSignUp(){
 		System.out.println("This is local method");
+	}
+	
+	public void signUpTest(){
+		System.out.println("This is signup test method");
 	}
 		
 			
